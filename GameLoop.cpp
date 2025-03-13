3ca827ae75155515431912b14cdf10da2decbd77
Oscar Duque <oscar.duque@enti.cat>
2025-03-13 11:05:30 +0100

    Gameloop
    gameloop y agays dinamicos

--- GameLoop.cpp
+++ GameLoop.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <cstdlib>
+#include <Windows.h>
 
 using namespace std;
 
-void main() 
+int main() 
 {
+    bool userPressedUp = false;
 
+    while (true)
+    {
+        if (GetAsyncKeyState(VK_UP) & 0x8000)
+        {
+            userPressedUp = true;
+            cout << "carlos tonto" << endl;
+            Sleep(30); // Agrega un pequeño delay para evitar uso excesivo de CPU 
+        }
+       
+        if (GetAsyncKeyState(VK_DOWN ) & 0x8000) 
+        {
+            userPressedUp = true;
+            cout << "jowi rustico" << endl;
+            Sleep(30); // Agrega un pequeño delay para evitar uso excesivo de CPU 
+        }
+       
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000) 
+        {
+            userPressedUp = true; 
+            cout << "enrique negro" << endl; 
+            Sleep(30); // Agrega un pequeño delay para evitar uso excesivo de CPU 
+        }
+        
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) 
+        {
+            userPressedUp = true;
+            cout << "lo pato" << endl;
+            Sleep(30); // Agrega un pequeño delay para evitar uso excesivo de CPU 
+        }
+        
+        
+    }
+
+    
 }
--- agaydinamicotamaño.cpp
+++ agaydinamicotamaño.cpp
@@ -36,51 +36,51 @@ int* newArray(int* dynamicAgay1D, int oldsize, int newsize)
 	return newDynamicAgay1D;
 }
 
-void main()
-{
-	int dynamicsize; 
-	cout << "di: " << endl;
-	cin >> dynamicsize;
-
-	int* dynamicAgay1D = new int[dynamicsize];
-
-	for (int i = 0; i < dynamicsize; ++i)
-	{
-		dynamicAgay1D[i] = i;
-	}
-	for (int i = 0; i < dynamicsize; ++i)
-	{
-		cout << dynamicAgay1D[i] << " ";
-	} cout << endl;
-
-
-	//paso a uno mas grande
-	int newSizeAgay;
-	cout << "di new Siz3: " << endl;
-	cin >> newSizeAgay;
-
-	int* newDynamicAgay1D = new int[newSizeAgay];
-	//copiar array antiguo al nuevo
-
-	for (int i = 0; i < dynamicsize; ++i)
-	{
-		newDynamicAgay1D[i] = dynamicAgay1D[i];
-	}
-
-	//destruir el array antiguo
-	delete[] dynamicAgay1D;
-
-	//asignar dynamic array, lo explica mas alante el manish
-	dynamicAgay1D = newDynamicAgay1D;
-
-	for (int i = 0; i < newSizeAgay; ++i)
-	{
-		cout << newDynamicAgay1D[i] << " ";
-	} cout << endl;
-
-
-	newArray(dynamicAgay1D, dynamicsize, newSizeAgay);
-	//preguntar a mi colega GPT como se hace un agay dinamico en el cual se aumente el tamaño del agay
-	//y preguntar como se haría para bajar el tamaño
-
-}
+//void main()
+//{
+//	int dynamicsize; 
+//	cout << "di: " << endl;
+//	cin >> dynamicsize;
+//
+//	int* dynamicAgay1D = new int[dynamicsize];
+//
+//	for (int i = 0; i < dynamicsize; ++i)
+//	{
+//		dynamicAgay1D[i] = i;
+//	}
+//	for (int i = 0; i < dynamicsize; ++i)
+//	{
+//		cout << dynamicAgay1D[i] << " ";
+//	} cout << endl;
+//
+//
+//	//paso a uno mas grande
+//	int newSizeAgay;
+//	cout << "di new Siz3: " << endl;
+//	cin >> newSizeAgay;
+//
+//	int* newDynamicAgay1D = new int[newSizeAgay];
+//	//copiar array antiguo al nuevo
+//
+//	for (int i = 0; i < dynamicsize; ++i)
+//	{
+//		newDynamicAgay1D[i] = dynamicAgay1D[i];
+//	}
+//
+//	//destruir el array antiguo
+//	delete[] dynamicAgay1D;
+//
+//	//asignar dynamic array, lo explica mas alante el manish
+//	dynamicAgay1D = newDynamicAgay1D;
+//
+//	for (int i = 0; i < newSizeAgay; ++i)
+//	{
+//		cout << newDynamicAgay1D[i] << " ";
+//	} cout << endl;
+//
+//
+//	newArray(dynamicAgay1D, dynamicsize, newSizeAgay);
+//	//preguntar a mi colega GPT como se hace un agay dinamico en el cual se aumente el tamaño del agay
+//	//y preguntar como se haría para bajar el tamaño
+//
+//}
